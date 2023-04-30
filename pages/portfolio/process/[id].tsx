1276181f484c5345e0f1c1ee8955bc90940f90cf
Robert Schmahl <69170803+rschm007@users.noreply.github.com>
2023-04-30 13:46:18 -0700

    removed link wrapper from pdfs

--- pages/portfolio/process/[id].tsx
+++ pages/portfolio/process/[id].tsx
@@ -22,7 +22,6 @@ export const ProcessDocPage = ({
         }
     }, [contentData])
 
-    console.log(pageContentData)
 
     const title = pageContentData.data.title;
 
@@ -38,25 +37,23 @@ export const ProcessDocPage = ({
                         <div className="flex flex-col items-center w-full space-y-6">
 
                             {pageContentData.urls.map((url, i) => (
-                                <Link href={url} key={i} className="flex flex-row w-full items-center justify-center">
+                                <>
                                     {url.includes("png" || "webp" || "jpg" || "jpeg") && (
-                                        <Image
-                                            className="object-cover w-7/12 h-full object-bottom relative brightness-100 group-hover:brightness-[0.25] transition-all ease-in-out delay-75 z-0 bg-darkGray "
-                                            src={url}
-                                            alt={pageContentData.data.title}
-                                            width={250}
-                                            height={500}
-                                            unoptimized
-                                            loading="lazy"
-                                        />
+                                        <Link href={url} key={i} className="flex flex-row w-full items-center justify-center">
+                                            <Image
+                                                className="object-cover w-7/12 h-full object-bottom relative brightness-100 group-hover:brightness-[0.25] transition-all ease-in-out delay-75 z-0 bg-darkGray "
+                                                src={url}
+                                                alt={pageContentData.data.title}
+                                                width={250}
+                                                height={500}
+                                                unoptimized
+                                                loading="lazy"
+                                            /></Link>
+
+
                                     )}
 
                                     {url.includes("pdf") && (
-                                        // <PDF
-                                        //     className="object-cover w-7/12 h-full object-bottom relative brightness-100 group-hover:brightness-[0.25] transition-all ease-in-out delay-75 z-0 bg-darkGray "
-                                        //     src={url}
-                                        // />
-
                                         <object
                                             className="object-cover w-9/12 h-full min-h-[70vh] object-bottom relative brightness-100 group-hover:brightness-[0.25] transition-all ease-in-out delay-75 z-0 bg-darkGray"
                                             data={url}
@@ -66,12 +63,9 @@ export const ProcessDocPage = ({
                                         />
 
                                     )}
-
-                                </Link>
-
+                                </>
                             ))
                             }
-
                         </div>
 
                     </section>
